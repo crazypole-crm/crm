8978ceb39fa3b2cedc796315feb44e75d7f1f39a
NiCrOmAnT <61577550+NiCrOmAnT@users.noreply.github.com>
2022-11-20 15:00:02 +0300

    Поправил получение курсов

--- backend/src/Training/Infrastructure/Query/Hydrator/CourseDataHydrator.php
+++ backend/src/Training/Infrastructure/Query/Hydrator/CourseDataHydrator.php
@@ -4,16 +4,17 @@ declare(strict_types=1);
 namespace App\Training\Infrastructure\Query\Hydrator;
 
 use App\Common\Infrastructure\Query\TypeConverter;
+use App\Training\App\Data\CourseData;
 use App\Training\App\Data\TrainingData;
 use App\Training\Infrastructure\Query\Table\CourseTable;
 use App\Training\Infrastructure\Query\Table\TrainingTable;
 
 class CourseDataHydrator
 {
-    public function hydrateRow(array $data): TrainingData
+    public function hydrateRow(array $data): CourseData
     {
         $id = TypeConverter::hydrateValue($data[CourseTable::COURSE_ID], CourseTable::EVENT_FIELDS[CourseTable::COURSE_ID]);
         $title = TypeConverter::hydrateValue($data[CourseTable::TITLE], CourseTable::EVENT_FIELDS[CourseTable::TITLE]);
-        return new TrainingData($id, $title);
+        return new CourseData($id, $title);
     }
 }
