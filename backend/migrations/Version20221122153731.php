a6b05d8e108a97c062264d03778f1253a2cab3db
NiCrOmAnT <61577550+NiCrOmAnT@users.noreply.github.com>
2022-11-22 20:07:24 +0300

    Поправил миграцию

--- backend/migrations/Version20221122153731.php
+++ backend/migrations/Version20221122153731.php
@@ -24,7 +24,7 @@ final class Version20221122153731 extends AbstractMigration
             (
                 id VARCHAR(64) PRIMARY KEY,
                 start_date DATETIME NOT NULL,
-                end_date DATETIME NOT NULL
+                end_date DATETIME NOT NULL,
                 trainer_id VARCHAR(64) NOT NULL
             );
 SQL
